7a2facd165f0e85e94a332a2441eb94706c65c96
GriszlyExe <grillmon_123@hotmail.com>
2023-08-24 15:00:51 +0700

    BaseMonkey

---
+++ src/Main/application.java
@@ -0,0 +1,17 @@
+package Main;
+
+import Monkey.BaseMonkey;
+import java.util.ArrayList;
+
+public class application {
+
+    public static ArrayList<BaseMonkey> getMonkeyContainer() {
+        return monkeyContainer;
+    }
+
+    public static ArrayList<BaseMonkey> monkeyContainer;
+
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/Monkey/BaseMonkey.java
@@ -0,0 +1,58 @@
+package Monkey;
+
+public class BaseMonkey {
+
+    private int maxHp ,hp, atk , def;
+
+    public BaseMonkey(int maxHp, int atk , int def){
+        this.maxHp = maxHp;
+        setHp(maxHp);
+        setAtk(atk);
+        setDef(def);
+    }
+
+    public int getMaxHp() {
+        return maxHp;
+    }
+
+    public BaseMonkey(){
+        setHp(6);
+        setAtk(2);
+        setDef(1);
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = (hp>0?hp:10);
+    }
+
+    public int getAtk() {
+        return atk;
+    }
+
+    public void setAtk(int atk) {
+        this.atk = (atk>0?atk:2);
+    }
+
+    public int getDef() {
+        return def;
+    }
+
+    public void setDef(int def) {
+        this.def = (def>0?def:3);
+    }
+
+    public void attack(BaseMonkey m){
+        if (getAtk()>m.getDef()){
+            int dmg = getAtk()-m.getDef();
+            m.setHp(m.getHp()-dmg);
+        }
+    }
+
+    public String getType(){
+        return this.getClass().getSimpleName();
+    }
+}
