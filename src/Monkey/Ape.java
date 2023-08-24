f17fc4cb08ce1aa0fdd03537264bbb23844c8e4d
GriszlyExe <grillmon_123@hotmail.com>
2023-08-24 15:47:28 +0700

    GameSystemComplete

---
+++ src/Monkey/Ape.java
@@ -0,0 +1,19 @@
+package Monkey;
+
+import logic.game.GameSystem;
+
+import java.util.ArrayList;
+
+public class Ape extends BaseMonkey{
+
+    public Ape(int maxHp, int atk, int def) {
+        super(maxHp, atk, def);
+    }
+
+    public void attack() {
+        ArrayList<BaseMonkey> monkeyContainer = GameSystem.getInstance().getMonkeyContainer();
+        for(BaseMonkey m:monkeyContainer){
+            m.setHp(m.getHp()-getAtk());
+        }
+    }
+}
--- src/logic/game/GameSystem.java
+++ src/logic/game/GameSystem.java
@@ -1,11 +1,16 @@
 package logic.game;
 
 import Monkey.BaseMonkey;
+import Monkey.MommyMonkey;
+import Monkey.MuscleMonkey;
+import Monkey.UgabugagaMonkey;
+
 import java.util.*;
 public class GameSystem {
-    private ArrayList<BaseMonkey> monkeyContainer;
-    private ArrayList<BaseMonkey> ApeContainer;
+    private final ArrayList<BaseMonkey> monkeyContainer;
     private int sp = 5;
+
+
     private boolean gameEnd;
     private static GameSystem instance = null;
 
@@ -16,7 +21,20 @@ public class GameSystem {
         return instance;
     }
     private GameSystem() {
+        monkeyContainer = new ArrayList<BaseMonkey>();
+        addMonkey();
+    }
 
+    public void addMonkey(){
+        BaseMonkey m1 = new BaseMonkey(100,15,10);
+        MuscleMonkey m2 = new MuscleMonkey(200,20,10);
+        MommyMonkey m3 = new MommyMonkey(80,0,10);
+        UgabugagaMonkey m4 = new UgabugagaMonkey(80,10,15);
+
+        getMonkeyContainer().add(m1);
+        getMonkeyContainer().add(m2);
+        getMonkeyContainer().add(m3);
+        getMonkeyContainer().add(m4);
     }
     public ArrayList<BaseMonkey> getMonkeyContainer() {
         return monkeyContainer;
@@ -32,17 +50,25 @@ public class GameSystem {
         this.gameEnd = gameEnd;
     }
 
-//    public void printCompetitorsStatus() {
-//        for(int i=0;i<allCompetitors.size();i++) {
-//            BaseCompetitor temp=allCompetitors.get(i);
-//            System.out.println(temp.getType()+" "+temp.getName()+" hp: "+temp.getHp()+" power: "+temp.getPower());
-//        }
-//    }
-//    public void removeDeadCompetitors() {
-//        for(int j=allCompetitors.size()-1;j>=0;j--) {
-//            if(allCompetitors.get(j).getHp()<=0) {
-//                allCompetitors.remove(j);
-//            }
-//        }
-//    }
+    public int getSp() {
+        return sp;
+    }
+
+    public void setSp(int sp) {
+        this.sp = sp;
+    }
+
+    public void printMonkeyStatus() {
+        for(int i=0;i<monkeyContainer.size();i++) {
+            BaseMonkey temp=getMonkeyContainer().get(i);
+            System.out.println(temp.getType() + " hp : " +temp.getHp() + " atk : " +temp.getAtk());
+        }
+    }
+    public void removeDeadCompetitors() {
+        for(int j=getMonkeyContainer().size()-1;j>=0;j--) {
+            if(getMonkeyContainer().get(j).getHp()<=0) {
+                getMonkeyContainer().remove(j);
+            }
+        }
+    }
 }
