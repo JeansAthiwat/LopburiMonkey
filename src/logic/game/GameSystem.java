e723ec11e6c721c9da009e6b5dd24df26135d4b6
JeansAthiwat <athiwat5146@gmail.com>
2023-08-26 15:55:00 +0700

    before anything brokennussy

--- src/logic/game/GameSystem.java
+++ src/logic/game/GameSystem.java
@@ -3,9 +3,12 @@ package logic.game;
 import monkey.*;
 
 import java.util.*;
+
 public class GameSystem {
-    private final ArrayList<BaseMonkey> monkeyContainer = new ArrayList<BaseMonkey>();;
-    private final ArrayList<BaseMonkey> apeContainer = new ArrayList<BaseMonkey>();;
+    private final ArrayList<BaseMonkey> monkeyContainer = new ArrayList<BaseMonkey>();
+    ;
+    private final ArrayList<BaseMonkey> apeContainer = new ArrayList<BaseMonkey>();
+    ;
     private int gameState = 0;
     private int sp = 5;
 
@@ -16,17 +19,18 @@ public class GameSystem {
     public static GameSystem getInstance() {
         return instance;
     }
+
     private GameSystem() {
-       // monkeyContainer = new ArrayList<BaseMonkey>();
+        // monkeyContainer = new ArrayList<BaseMonkey>();
         //apeContainer = new ArrayList<BaseMonkey>();
         addApe();
     }
 
-    public void addMonkey(){
-        BaseMonkey m1 = new BaseMonkey(100,15,10);
-        MuscleMonkey m2 = new MuscleMonkey(200,20,10);
-        MommyMonkey m3 = new MommyMonkey(80,0,10);
-        UgabugagaMonkey m4 = new UgabugagaMonkey(80,10,15);
+    public void addMonkey() {
+        BaseMonkey m1 = new BaseMonkey(100, 15, 10);
+        MuscleMonkey m2 = new MuscleMonkey(200, 20, 10);
+        MommyMonkey m3 = new MommyMonkey(80, 0, 10);
+        UgabugagaMonkey m4 = new UgabugagaMonkey(80, 10, 15);
 
         getMonkeyContainer().add(m1);
         getMonkeyContainer().add(m2);
@@ -37,18 +41,52 @@ public class GameSystem {
     public void addApe() {
         for (int i = 0; i < 3; i++) getApeContainer().add(new Ape(200, 30, 10));
     }
+
     public ArrayList<BaseMonkey> getMonkeyContainer() {
         return monkeyContainer;
     }
 
     public boolean isGameEnd() {
-        if(monkeyContainer.size()<=1) {
-            gameEnd=true;
+        if (monkeyContainer.size() <= 1) {
+            gameEnd = true;
         }
         return gameEnd;
     }
+
     public void setGameEnd(boolean gameEnd) {
         this.gameEnd = gameEnd;
+    }
+
+
+    public ArrayList<BaseMonkey> getApeContainer() {
+        return apeContainer;
+    }
+
+    public void printMonkeyStatus() {
+        for (int i = 0; i < monkeyContainer.size(); i++) {
+            BaseMonkey temp = getMonkeyContainer().get(i);
+            System.out.println(temp.toString());
+        }
+    }
+
+    public void removeDeadEntity(ArrayList<BaseMonkey> entityContainer) {
+        for (int i = 0; i < entityContainer.size(); i++) {
+            BaseMonkey monkey = entityContainer.get(i);
+            if (monkey.getHp() <= 0) {
+                entityContainer.remove(monkey);
+                showDeadMessage(monkey, i);
+            }
+        }
+    }
+
+    public void showDeadMessage(BaseMonkey monkey, int index) {
+
+        if (monkey.getType().equals("Ape")) {
+            System.out.println("Monkey takes down one enemy ape!");
+            System.out.println("Ape " + "<" + index + "> has fallen!");
+        } else {
+            System.out.println("your " + monkey.getType() + " <" + index + ">" + " has fallen. Press 'F' to pay them respects.");
+        }
     }
 
     public int getSp() {
@@ -57,37 +95,6 @@ public class GameSystem {
 
     public void setSp(int sp) {
         this.sp = sp;
-    }
-
-    public ArrayList<BaseMonkey> getApeContainer() {
-        return apeContainer;
-    }
-
-    public void printMonkeyStatus() {
-        for(int i=0;i<monkeyContainer.size();i++) {
-            BaseMonkey temp=getMonkeyContainer().get(i);
-            System.out.println(temp.toString());
-        }
-    }
-
-    public void removeDeadEntity(ArrayList<BaseMonkey> entityContainer) {
-        for(int i = 0;i < entityContainer.size();i++){
-            BaseMonkey monkey = entityContainer.get(i);
-            if(monkey.getHp() <= 0){
-                entityContainer.remove(monkey);
-                showDeadMessage(monkey, i);
-            }
-        }
-    }
-    public void showDeadMessage(BaseMonkey monkey, int index){
-
-        if(monkey.getType().equals("Ape")){
-            System.out.println("Monkey takes down one enemy ape!");
-            System.out.println("Ape "+"<"+index+"> has fallen!");
-        }
-        else{
-            System.out.println("your "+ monkey.getType()+" <"+index+">"+" has fallen. Press 'F' to pay them respects.");
-        }
     }
 
 }
--- src/monkey/UgabugagaMonkey.java
+++ src/monkey/UgabugagaMonkey.java
@@ -1,21 +1,19 @@
 package monkey;
 
 public class UgabugagaMonkey extends BaseMonkey {
-    private final int debuff,heal;
+    private final int DEBUFF = 1, HEAL = 10;
     public UgabugagaMonkey(int maxHp, int atk, int def) {
         super(maxHp, atk, def);
-        this.debuff = 1;
-        this.heal = 10;
     }
 
     @Override
     public void attack(BaseMonkey m) {
         super.attack(m);
-        m.setAtk(m.getAtk()-debuff);
-        m.setDef(m.getDef()-debuff);
+        m.setAtk(m.getAtk()- DEBUFF);
+        m.setDef(m.getDef()- DEBUFF);
     }
 
     public void heal(BaseMonkey m){
-        m.setHp(m.getMaxHp()<m.getHp()+heal?getMaxHp():m.getHp()+heal);
+        m.setHp(m.getMaxHp()<m.getHp()+ HEAL ?getMaxHp():m.getHp()+ HEAL);
     }
 }
